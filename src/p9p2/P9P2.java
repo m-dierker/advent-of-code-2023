6e0bad27e34d2667a13a07764a6b704b47cd498e
Matthew Dierker <mvd7793@gmail.com>
2023-12-24 13:50:16 -0800

    day9

---
+++ src/p9p2/P9P2.java
@@ -0,0 +1,39 @@
+package p9p2;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P9P2 {
+  public static void main(String[] args) throws Exception {
+    List<String> lines = Files.readAllLines(Paths.get("./data/9.txt"));
+
+    long sum = 0;
+    for (String origSeq : lines) {
+      sum += solve(Arrays.stream(origSeq.split(" ")).map(str -> Integer.parseInt(str)).toList());
+    }
+    System.out.println("sum: " + sum);
+  }
+
+  private static int solve(List<Integer> seq) {
+    if (allZeroes(seq)) {
+      return 0;
+    }
+    List<Integer> diffs = new ArrayList<>(seq.size() - 1);
+    for (int i = 0; i < seq.size() - 1; i++) {
+      diffs.add(seq.get(i + 1) - seq.get(i));
+    }
+    return seq.get(0) - solve(diffs);
+  }
+
+  private static boolean allZeroes(List<Integer> seq) {
+    for (int i : seq) {
+      if (i != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
